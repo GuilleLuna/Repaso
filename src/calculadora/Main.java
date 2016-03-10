803ec499a897aa9e50365a885ff8d6da4dea7324
Guille <Guille@192.168.1.11>
2016-03-10 20:35:24 +0100

    #1

---
+++ src/calculadora/Main.java
@@ -0,0 +1,15 @@
+
+package calculadora;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+       
+        Calculando cal1 = new Calculando();
+        
+        cal1.add(2.4,3.4);
+    }
+
+}
